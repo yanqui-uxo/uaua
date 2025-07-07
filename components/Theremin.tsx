e8578f6d7ab9d87601409329720dc3b6c7b5839d
yanqui-uxo <zdiesing@icloud.com>
2025-07-07 15:43:04 -0700

    Functional theremin

--- components/Theremin.tsx
+++ components/Theremin.tsx
@@ -7,55 +7,62 @@ import {
   TouchData,
 } from "react-native-gesture-handler";
 import { AudioContext, GainNode, OscillatorNode } from "react-native-audio-api";
-import { Button, StyleSheet } from "react-native";
-import { runOnUI } from "react-native-reanimated";
+import { StyleSheet } from "react-native";
 import { Canvas, Circle } from "@shopify/react-native-skia";
 
 type Tone = { oscillator: OscillatorNode; gainNode: GainNode };
 
-// TODO: use Pressable instead of GestureDetector
 export default function Theremin() {
   const audioContextRef = useRef<AudioContext>(new AudioContext());
 
   const [touches, setTouches] = useState<TouchData[]>([]);
   const widthRef = useRef<number | null>(null);
   const heightRef = useRef<number | null>(null);
-  const tones = useRef<Record<number, Tone>>({});
+  const tonesRef = useRef<Map<number, Tone>>(new Map());
 
   function onTouchRemove(e: GestureTouchEvent, manager: GestureStateManager) {
-    e.changedTouches.forEach((t) => {
-      const { oscillator, gainNode } = tones.current[t.id];
-      gainNode.disconnect();
-      oscillator.disconnect();
-      delete tones.current[t.id];
-    });
-    if (e.numberOfTouches === 0) {
-      runOnUI(() => manager.end())();
-      setTouches([]);
+    const changedIds = e.changedTouches.map((t) => t.id);
+    const confirmedTouches = e.allTouches.filter(
+      (t) => !changedIds.includes(t.id)
+    );
+    setTouches(confirmedTouches);
+
+    if (confirmedTouches.length === 0) {
+      // sanity check
+      tonesRef.current = new Map();
+      audioContextRef.current.close();
+      audioContextRef.current = new AudioContext();
     } else {
-      setTouches(e.allTouches);
+      const confirmedIds = confirmedTouches.map((t) => t.id);
+      [...tonesRef.current.entries()]
+        .filter(([id, _]) => !confirmedIds.includes(id))
+        .forEach(([id, tone]) => {
+          tone.gainNode.disconnect();
+          tone.oscillator.disconnect();
+          tonesRef.current.delete(id);
+        });
     }
   }
 
   function xToFrequency(x: number): number {
-    return x * 2;
+    return x * 1.5;
   }
   function yToGain(y: number): number {
     const height = heightRef.current!;
     return (height - y) / height;
   }
 
-  const gesture = Gesture.Manual()
+  const gesture = Gesture.Pan()
     .onTouchesDown((e, manager) => {
-      runOnUI(() => manager.activate())();
       setTouches(e.allTouches);
       e.changedTouches.forEach((t) => {
         const oscillator = audioContextRef.current.createOscillator();
+        const gainNode = audioContextRef.current.createGain();
+        tonesRef.current.set(t.id, { oscillator, gainNode });
         oscillator.frequency.setValueAtTime(
           xToFrequency(t.x),
           audioContextRef.current.currentTime
         );
-        const gainNode = audioContextRef.current.createGain();
         gainNode.gain.setValueAtTime(
           yToGain(t.y),
           audioContextRef.current.currentTime
@@ -63,19 +70,18 @@ export default function Theremin() {
         oscillator.connect(gainNode);
         gainNode.connect(audioContextRef.current.destination);
         oscillator.start();
-        tones.current[t.id] = { oscillator, gainNode };
       });
     })
     .onTouchesMove((e) => {
       setTouches(e.allTouches);
 
       e.changedTouches.forEach((t) => {
-        const { oscillator, gainNode } = tones.current[t.id];
-        oscillator.frequency.setValueAtTime(
+        const tone = tonesRef.current.get(t.id);
+        tone?.oscillator.frequency.setValueAtTime(
           xToFrequency(t.x),
           audioContextRef.current.currentTime
         );
-        gainNode.gain.setValueAtTime(
+        tone?.gainNode.gain.setValueAtTime(
           yToGain(t.y),
           audioContextRef.current.currentTime
         );
@@ -86,30 +92,20 @@ export default function Theremin() {
     .runOnJS(true);
 
   return (
-    <>
-      <GestureDetector gesture={gesture}>
-        <Canvas
-          style={styles.canvas}
-          onLayout={(e) => {
-            const { width, height } = e.nativeEvent.layout;
-            widthRef.current = width;
-            heightRef.current = height;
-          }}
-        >
-          {touches.map((t) => (
-            <Circle cx={t.x} cy={t.y} r={50} key={t.id}></Circle>
-          ))}
-        </Canvas>
-      </GestureDetector>
-      <Button
-        title="SILENCE"
-        onPress={() => {
-          audioContextRef.current.close();
-          audioContextRef.current = new AudioContext();
-          tones.current = {};
+    <GestureDetector gesture={gesture}>
+      <Canvas
+        style={styles.canvas}
+        onLayout={(e) => {
+          const { width, height } = e.nativeEvent.layout;
+          widthRef.current = width;
+          heightRef.current = height;
         }}
-      />
-    </>
+      >
+        {touches.map((t) => (
+          <Circle cx={t.x} cy={t.y} r={50} key={t.id}></Circle>
+        ))}
+      </Canvas>
+    </GestureDetector>
   );
 }
 
@@ -117,11 +113,5 @@ const styles = StyleSheet.create({
   canvas: {
     flex: 1,
     backgroundColor: "skyblue",
-    height: 1000,
-    flexBasis: 500,
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
   },
 });
